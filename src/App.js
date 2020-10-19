089ed74211e5427f753151430a90e65a5664f05a
KriszProg <krisz.codecool@gmail.com>
2020-10-19 11:49:38 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = (props) => {
+  return (
+    <div className="App">
+      <h1>Harry Potter</h1>
+    </div>
+  );
+}
+
+export default App;
