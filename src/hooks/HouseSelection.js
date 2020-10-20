e6eec7e2f17d66a7378b4235ccd3ba3a5f53a00d
MMaGH <misk.mardi@gmail.com>
2020-10-20 16:39:40 +0200

    removed flickering when first hovering on an element on houseSelector

--- src/hooks/HouseSelection.js
+++ src/hooks/HouseSelection.js
@@ -11,36 +11,40 @@ export default function HouseSelection() {
 
 
     function changeBackgroundToNormal(e) {
-        let backgroundNode = document.querySelector(".houseBackground")
+        let backgroundNode = document.querySelector("#background")
         backgroundNode.style.backgroundImage = `url(${hpHouseMain})`;
     }
 
     function changeBackgroundToGryffindor(e) {
-        let backgroundNode = document.querySelector(".houseBackground")
+        let backgroundNode = document.querySelector("#background")
         backgroundNode.style.backgroundImage = `url(${hpHouseGryffindor})`;
     }
 
     function changeBackgroundToSlytherin(e) {
-        let backgroundNode = document.querySelector(".houseBackground")
+        let backgroundNode = document.querySelector("#background")
         backgroundNode.style.backgroundImage = `url(${hpHouseSlytherin})`;
     }
 
     function changeBackgroundToHufflepuff(e) {
-        let backgroundNode = document.querySelector(".houseBackground")
+        let backgroundNode = document.querySelector("#background")
         backgroundNode.style.backgroundImage = `url(${hpHouseHufflepuff})`;
     }
 
     function changeBackgroundToRavenclaw(e) {
-        let backgroundNode = document.querySelector(".houseBackground")
+        let backgroundNode = document.querySelector("#background")
         backgroundNode.style.backgroundImage = `url(${hpHouseRavenclaw})`;
     }
 
+
     return (
         <div className="houseBackground" style={{backgroundImage:`url(${hpHouseMain})`}}>
-            <Link to="/gryffindor" className="houseDiv" onMouseOver={changeBackgroundToGryffindor} onMouseLeave={changeBackgroundToNormal}></Link>
-            <Link to="/slytherin" className="houseDiv" onMouseOver={changeBackgroundToSlytherin} onMouseLeave={changeBackgroundToNormal}></Link>
-            <Link to="/hufflepuff" className="houseDiv" onMouseOver={changeBackgroundToHufflepuff} onMouseLeave={changeBackgroundToNormal}></Link>
-            <Link to="/ravenclaw" className="houseDiv" onMouseOver={changeBackgroundToRavenclaw} onMouseLeave={changeBackgroundToNormal}></Link>
+            <div id="background" className="houseBackground" style={{backgroundImage:`url(${hpHouseMain})`}}>
+                <Link to="/gryffindor" className="houseDiv" onMouseOver={changeBackgroundToGryffindor} onMouseLeave={changeBackgroundToNormal}></Link>
+                <Link to="/slytherin" className="houseDiv" onMouseOver={changeBackgroundToSlytherin} onMouseLeave={changeBackgroundToNormal}></Link>
+                <Link to="/hufflepuff" className="houseDiv" onMouseOver={changeBackgroundToHufflepuff} onMouseLeave={changeBackgroundToNormal}></Link>
+                <Link to="/ravenclaw" className="houseDiv" onMouseOver={changeBackgroundToRavenclaw} onMouseLeave={changeBackgroundToNormal}></Link>
+            </div>
         </div>
+        
     )
 }
