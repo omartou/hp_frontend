da43dcd16a5c84f2e6b700bf2947d316870bc21c
KriszProg <krisz.codecool@gmail.com>
2020-10-21 07:38:55 +0200

    refactor HomePage with using styled-components

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,33 @@
+// import React from 'react'
+// import "./HomePage.css";
+// import {
+//     Link
+// } from "react-router-dom";
+// import hogwarts from '../images/castle.png';
+// import ministry from '../images/ministry.png';
+
+
+// export default function HomePage() {
+//     return (
+//         <div className="homePageContainer">
+//             <Link to="/hogwarts" className="hogwarts" style={{backgroundImage: `url(${hogwarts})`}}></Link>
+//             <Link to="/ministry" className="ministry" style={{backgroundImage: `url(${ministry})`}}></Link>
+//         </div>
+//     )
+// }
+
 import React from 'react'
 import "./HomePage.css";
-import {
-    Link
-} from "react-router-dom";
-import hogwarts from '../images/castle.png';
-import ministry from '../images/ministry.png';
+import Logo from "./Logo";
+import {MinistryImage, HogwartsImage, HomePageContainer} from "../styled_components/index";
 
 
 export default function HomePage() {
     return (
-        <div className="homePageContainer">
-            <Link to="/hogwarts" className="hogwarts" style={{backgroundImage: `url(${hogwarts})`}}></Link>
-            <Link to="/ministry" className="ministry" style={{backgroundImage: `url(${ministry})`}}></Link>
-        </div>
+        <HomePageContainer>
+            <Logo/>
+            <HogwartsImage to="/hogwarts"></HogwartsImage>
+            <MinistryImage to="/ministry"></MinistryImage>
+        </HomePageContainer>
     )
 }
