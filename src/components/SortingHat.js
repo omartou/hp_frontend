df7fdeecec0929b784ef48b6a29e19a3f42f08e1
MMaGH <misk.mardi@gmail.com>
2020-11-18 15:37:06 +0100

    add pictures to fields

--- src/components/SortingHat.js
+++ src/components/SortingHat.js
@@ -1,26 +1,39 @@
 import React from 'react'
 import {Hat, Banner, SortingHatContainer, SortingHatInfo, ChangableBanner} from '../styled_components/SortingHatComponents';
 import Content from "../styled_components/Content";
+import GryffindorBanner from "../images/GryffindorBanner.png";
+import SlytherinBanner from "../images/SlytherinBanner.png";
+import HufflepuffBanner from "../images/HufflepuffBanner.png";
+import RavenclawBanner from "../images/RavenclawBanner.png";
+import SortingHatTalks from "../images/SortingHatTalks.png";
+
 
 
 export default function SortingHat(props) {
 
     const getHouse = function() {
         let targetField = document.querySelector("#houseField");
-        let ChangableBanners = document.querySelectorAll(".changableBanner");
+        let changableBanners = document.querySelectorAll(".changableBanner");
+        let hat = document.querySelector("#hat")
         let house = getRandomHouse();
 
         targetField.innerText = house;
         targetField.style.opacity = "100%";
-        // red should be changed to house
-        changeBanners(ChangableBanners, "red");
+        
+        hat.style.backgroundImage = `url(${SortingHatTalks})`;
+        hat.style.pointerEvents = "none";
+        changeBanners(changableBanners, house);
     }
 
     const changeBanners = function(banners, newBanner) {
+        let bannerImgs = {"Gryffindor": GryffindorBanner,
+                          "Slytherin" : SlytherinBanner,
+                          "Hufflepuff" : HufflepuffBanner,
+                          "Ravenclaw" : RavenclawBanner};
         for (let banner of banners) {
             // this line should be uncommented and next one should be removed
-            //banner.style.backgroundImage = `url(${newBanner})`
-            banner.style.backgroundColor = newBanner;
+            banner.style.backgroundImage = `url(${bannerImgs[newBanner]})`
+            //banner.style.backgroundColor = newBanner;
             banner.style.opacity = "100%"
         }
     }
@@ -37,7 +50,7 @@ export default function SortingHat(props) {
             <Banner>
                 <ChangableBanner className="changableBanner"></ChangableBanner>
             </Banner>
-            <Hat onClick={getHouse}>
+            <Hat id="hat" onClick={getHouse}>
                 <SortingHatInfo id="houseField"></SortingHatInfo>
             </Hat>
             <Banner>
--- src/styled_components/SortingHatComponents.js
+++ src/styled_components/SortingHatComponents.js
@@ -1,41 +1,59 @@
 import styled from 'styled-components';
+import HogwartsBanner from "../images/HogwartsBanner.png";
+import SortingHat from "../images/SortingHat.png";
+import SortingHatOnHover from "../images/SortingHatOnHover.png";
 
 const Banner = styled("div")`
     height : 100%;
     width : 20%;
-    /*background-image*/
-    background-color : beige;
+    background-image : url(${HogwartsBanner});
     display: inline-block;
+    background-repeat: no-repeat;
+    background-position-x: center;
+    background-size: contain;
 `;
 
 
 const ChangableBanner = styled("div")`
     height : 100%;
     width : 100%;
-    transition: 2000ms;
+    transition: 3000ms;
     opacity: 0%;
+    background-repeat: no-repeat;
+    background-position-x: center;
+    background-size: contain;
 `;
 
 
 const Hat = styled("div")`
     height : 100%;
     width: 60%;
-    background-color: burlywood;
+    background-image : url(${SortingHat});
     display: inline-block;
+    background-repeat: no-repeat;
+    background-position-x: center;
+    background-position-y: top;
+    background-size: 60%;
+    transition: 100ms;
+    &:hover {
+        background-image : url(${SortingHatOnHover});
+        transition: 100ms;
+    }
 `;
 
 const SortingHatContainer = styled("div")`
     height : 70vh;
     width: 100vw;
-    background-color: black;
 `;
 
 const SortingHatInfo = styled("div")`
-    height : 30%;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    color: goldenrod;
+    -webkit-text-stroke: 3px black;
+    height : 20%;
     width : inherit;
     position: absolute;
     bottom : 0px;
-    border: 1px solid pink;
     font-size: 600%;
     transition: 1000ms;
     opacity: 0%;
