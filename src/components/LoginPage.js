2ff21ac08ca44a7b21a51048a1f235d8e141f09b
KriszProg <krisz.codecool@gmail.com>
2020-12-02 10:52:28 +0100

    modify Login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from "react";
 import Content from "../styled_components/Content";
 import { Redirect } from "react-router-dom";
 import axios from "axios";
-// import cookie from "react-cookie";
 
 export default function LoginPage(props) {
-  // const [isLoggedIn, setLoggedIn] = useState(false);
   const [authorization, setAuthorization] = useState({});
 
   const submitHandler = (e) => {
@@ -18,10 +16,6 @@ export default function LoginPage(props) {
         password: password,
       })
       .then((res) => {
-        // if (res.data === "Login successful!") {
-        //   setLoggedIn(true);
-        // }
-        // console.log(res.data);
         setAuthorization(res.data)
         console.log(res.data.status);
       });
@@ -31,13 +25,9 @@ export default function LoginPage(props) {
     props.setTitle("Login");
   }, [props]);
 
-  // if (isLoggedIn) {
   if (authorization.status === "Login successful!") {
     console.log("Muhaha... we have token: " + authorization.token);
     document.cookie = `token=${authorization.token}`;
-    // console.log(document.cookie);
-    // const token = document.cookie.split("=")[1];
-    // console.log("token from cookie: " + token); 
     return <Redirect to="/" />;
   } else {
     return (
